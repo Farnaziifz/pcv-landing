4c9db4bc5aa6aef48c21e1882fb14f3de056cdf3
farnaz <ffarzipour92@gmail.com>
2024-08-20 11:07:18 +0330

    finished

--- src/views/FinishedPage/index.js
+++ src/views/FinishedPage/index.js
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router-dom";
 function FinishedPgae() {
-	return <p>salam</p>;
+	const navigate = useNavigate();
+	const goToMain = () => {
+		navigate("/login");
+		window.localStorage.removeItem("nationalId");
+		window.localStorage.removeItem("phoneNumber");
+	};
+	return (
+		<main className="w-[360px] mt-10 p-6 bg-white shadow-md rounded-md text-center">
+			<p className="text-xl mb-4 font-bold"> با تشکر.</p>
+			<p className="mb-3">اطلاعات شما با موفقیت ثبت شد.</p>
+			کارشناسان ما همراه شما هستند.
+			<button
+				className="bg-primary text-white px-3 py-1 mt-5 rounded"
+				onClick={goToMain}
+			>
+				بازگشت به صفحه اصلی
+			</button>
+		</main>
+	);
 }
 
 export default FinishedPgae;
